da4dcd0ab170fb341b18509e8e5e3bf6a910dc1d
Mark Astley <deadlybulb@orbital.enterprises>
2017-01-31 20:24:36 -0500

    - add support for refresh of EVE access tokens

--- src/main/java/enterprises/orbital/oauth/EVEAuthHandler.java
+++ src/main/java/enterprises/orbital/oauth/EVEAuthHandler.java
@@ -7,6 +7,7 @@ import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 import com.github.scribejava.core.builder.ServiceBuilder;
+import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.github.scribejava.core.oauth.OAuth20Service;
 
 /**
@@ -20,6 +21,7 @@ public class EVEAuthHandler {
                              String secretKey,
                              String callback,
                              String scope,
+                             String state,
                              HttpServletRequest req)
     throws IOException {
     AuthUtil.prepAuthFlow(req);
@@ -28,10 +30,27 @@ public class EVEAuthHandler {
       // Construct an OAuth request for EVE.
       ServiceBuilder builder = new ServiceBuilder().responseType("code").apiKey(clientID).apiSecret(secretKey).callback(callback);
       if (scope != null && !scope.isEmpty()) builder = builder.scope(scope);
+      if (state != null && !state.isEmpty()) builder = builder.state(state);
       OAuth20Service service = builder.build(EVEApi.instance());
       return service.getAuthorizationUrl(null);
     } catch (Exception e) {
       log.log(Level.SEVERE, "Error while attempting EVE authentication: ", e);
+      return null;
+    }
+  }
+
+  public static OAuth2AccessToken doRefresh(
+                                            String clientID,
+                                            String secretKey,
+                                            String refreshToken)
+    throws IOException {
+    try {
+      // Construct a refresh request to EVE.
+      ServiceBuilder builder = new ServiceBuilder().apiKey(clientID).apiSecret(secretKey);
+      OAuth20Service service = builder.build(EVEApi.instance());
+      return service.refreshAccessToken(refreshToken);
+    } catch (Exception e) {
+      log.log(Level.SEVERE, "Error while attempting to refresh access token: ", e);
       return null;
     }
   }
